453b5204fd2bebb5525b1d6f8a790a50e8368f95
Øyvind Raddum Berg <elacin@gmail.com>
2023-06-16 01:38:43 +0200

    Create tables

--- init/install.sh
+++ init/install.sh
@@ -8,3 +8,6 @@ psql <<- SHELL
 SHELL
 #cd /data
 psql -d Adventureworks < /docker-entrypoint-initdb.d/data/install.sql
+
+# this should have had a database by itself, but let's be lazy for now
+psql -d Adventureworks < /docker-entrypoint-initdb.d/data/test-tables.sql
