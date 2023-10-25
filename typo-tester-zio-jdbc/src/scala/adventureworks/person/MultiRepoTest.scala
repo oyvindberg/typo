69bd66c0a6ffb91a7b1c1a6fb8f51415aff2e671
jules Ivanic <jules.ivanic@gmail.com>
2023-10-25 15:03:36 +0400

    Fix test

--- typo-tester-zio-jdbc/src/scala/adventureworks/person/MultiRepoTest.scala
+++ typo-tester-zio-jdbc/src/scala/adventureworks/person/MultiRepoTest.scala
@@ -5,7 +5,7 @@ import adventureworks.person.addresstype.*
 import adventureworks.person.businessentityaddress.*
 import adventureworks.person.person.*
 import adventureworks.public.Name
-import zio.ZIO
+import zio.{Chunk, ZIO}
 import zio.jdbc.ZConnection
 import zio.prelude.ForEachOps // necessary for 2.12
 
@@ -24,25 +24,25 @@ case class PersonWithAddressesRepo(
     *
     * and the given addresses are attached with the chosen type
     */
-  def upsert(pa: PersonWithAddresses): ZIO[ZConnection, Throwable, List[BusinessentityaddressRow]] = {
+  def upsert(pa: PersonWithAddresses): ZIO[ZConnection, Throwable, Chunk[BusinessentityaddressRow]] = {
     for {
       // update person
       _ <- personRepo.upsert(pa.person)
       // update stored addresses
-      _ <- pa.addresses.values.toList.forEach { address => addressRepo.update(address) }
+      _ <- Chunk.fromIterable(pa.addresses.values).forEach { address => addressRepo.update(address) }
       // addresses are stored in `PersonWithAddress` by a `Name` which means what type of address it is.
       // this address type is stored in addresstypeRepo.
       // In order for foreign keys to align, we need to translate from names to ids, and create rows as necessary
-      oldStoredAddressTypes <- addresstypeRepo.select.where(r => r.name in pa.addresses.keys.toArray).toList
-      currentAddressesWithAddresstype <- pa.addresses.toList.forEach { case (addressTypeName, wanted) =>
+      oldStoredAddressTypes <- addresstypeRepo.select.where(r => r.name in pa.addresses.keys.toArray).toChunk
+      currentAddressesWithAddresstype <- Chunk.fromIterable(pa.addresses).forEach { case (addressTypeName, wanted) =>
         oldStoredAddressTypes.find(_.name == addressTypeName) match {
           case Some(found) => ZIO.succeed((found.addresstypeid, wanted))
-          case None        => addresstypeRepo.insert(AddresstypeRowUnsaved(name = addressTypeName)).map(row => (row.addresstypeid, wanted))
+          case None        => addresstypeRepo.insert(AddresstypeRowUnsaved(name = addressTypeName)).map(row => (row.updatedKeys.head.addresstypeid, wanted))
         }
       }
       currentAddressesByAddresstype = currentAddressesWithAddresstype.toMap
       // discover existing addresses attached to person
-      oldAttachedAddresses <- businessentityAddressRepo.select.where(x => x.businessentityid === pa.person.businessentityid).toList
+      oldAttachedAddresses <- businessentityAddressRepo.select.where(x => x.businessentityid === pa.person.businessentityid).toChunk
       // unattach old attached rows
       _ <- oldAttachedAddresses.forEach { ba =>
         currentAddressesByAddresstype.get(ba.addresstypeid) match {
@@ -56,7 +56,7 @@ case class PersonWithAddressesRepo(
           case None =>
             businessentityAddressRepo.insert(
               BusinessentityaddressRowUnsaved(pa.person.businessentityid, address.addressid, addresstypeId)
-            )
+            ).map(_.updatedKeys.head)
         }
       }
     } yield currentAttachedAddresses
